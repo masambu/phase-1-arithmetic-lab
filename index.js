0534e47fa24012e3037366e860ebb2d64b6aff30
Kelvin <kevohmasambu@gmail.com>
2023-12-11 14:17:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+
+
+
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+
+const set = (4, 8, 12, 16, 20);
+const max = Math.max(set);
+console.log(max);
